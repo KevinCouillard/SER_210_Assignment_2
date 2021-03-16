1081442aa6f36efdcfed8e2a7e4445fd211a7d57
Hephzibah Rajan <rajanhephzibah@gmail.com>
2021-03-16 01:12:59 -0400

    About page added

--- app/src/main/java/edu/quinnipiac/ser210/triviaapp/MainActivity.java
+++ app/src/main/java/edu/quinnipiac/ser210/triviaapp/MainActivity.java
@@ -1,10 +1,12 @@
 package edu.quinnipiac.ser210.triviaapp;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.Menu;
+import android.view.MenuItem;
 import android.view.View;
 import android.os.Bundle;
 
@@ -14,12 +16,31 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        setSupportActionBar(findViewById(R.id.toolbar));
     }
 
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
         getMenuInflater().inflate(R.menu.question_options,menu);
         return super.onCreateOptionsMenu(menu);
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(@NonNull MenuItem item) {
+        int id = item.getItemId();
+        switch (id) {
+            case R.id.change_color:
+                //options to change the background color
+                break;
+            case R.id.info:
+                Intent intent = new Intent(MainActivity.this,AboutPage.class);
+                startActivity(intent);
+                //information about the developer
+                break;
+            default: return super.onOptionsItemSelected(item);
+
+        }
+        return super.onOptionsItemSelected(item);
     }
 
     public void startGame(View view) {
